154e8aba4b411f71946d1c2a82a865de2ce300b5
thundernoodle <thundernoodle@student.edu>
2017-09-15 17:55:27 +0200

    changes to models

--- library/book/models.py
+++ library/book/models.py
@@ -3,9 +3,13 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=300)
     author = models.CharField(max_length=400, blank=True, null=True )
+    publishing_line = models.CharField(max_length=200, blank=True, null=True)
     year = models.IntegerField(blank=True, null=True)
     type = models.CharField(max_length=70, blank=True, null=True)
     cover = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
 
 class Owner(models.Model):
     book_title = models.ForeignKey
@@ -13,3 +17,6 @@ class Owner(models.Model):
     rented = models.NullBooleanField(default=False, blank=True)
     rented_by = models.CharField(max_length=200, blank=True, null=True)
     rented_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.owner
